9d6a6ee613bc0e089a460071be2dcc229954d228
sebasilvanavea <sebastian.silvanavea@gmail.com>
2026-04-06 15:32:51 -0400

    Initial Commit

---
+++ GuitarMBS/GuitarMBSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarMBSApp.swift
+//  GuitarMBS
+//
+//  Created by Sebastian Silva Navea on 06-04-26.
+//
+
+import SwiftUI
+
+@main
+struct GuitarMBSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
